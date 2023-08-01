542cbcb9e25b92a41b791531fd04f01e12fd732e
Kaua Matheus <kauzinhor@gmail.com>
2023-07-31 21:49:05 -0300

    Committt

---
+++ app/src/main/java/devandroid/kauamatheus/listadepedidos/Model/order_item.java
@@ -0,0 +1,67 @@
+package devandroid.kauamatheus.listadepedidos.Model;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import devandroid.kauamatheus.listadepedidos.R;
+
+public class order_item extends RecyclerView.Adapter<order_item.RecyclerTesteViewHolder> {
+
+    public static Interface.ClickRecyclerView_Interface clickRecyclerViewInterface;
+    Context mctx;
+    private List<Pedido> mList;
+
+    public order_item(Context ctx, List<Pedido> list, Interface.ClickRecyclerView_Interface clickRecyclerViewInterface) {
+        this.mctx = ctx;
+        this.mList = list;
+        this.clickRecyclerViewInterface = clickRecyclerViewInterface;
+    }
+
+    @Override
+    public RecyclerTesteViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_order_item, viewGroup, false);
+        return new RecyclerTesteViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerTesteViewHolder viewHolder, int i) {
+        Pedido pedido = mList.get(i);
+
+        viewHolder.viewNome.setText(pedido.getNome());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+
+    protected class RecyclerTesteViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView viewNome;
+
+        public RecyclerTesteViewHolder(final View itemView) {
+            super(itemView);
+
+            viewNome = (TextView) itemView.findViewById(R.id.TextViewNome);
+
+            //Setup the click listener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    clickRecyclerViewInterface.onCustomClick(mList.get(getLayoutPosition()));
+
+                }
+            });
+        }
+    }
+}
